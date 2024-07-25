e9abe80918b092200f2a10f1f4d22fae5ce3f5ed
Brent Miller <foliosus@foliosus.com>
2024-07-24 21:40:24 -0700

    Gonna need a reasonable Time.zone so the calendar looks right

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  around_action :set_user_time_zone
+
+  private def set_user_time_zone
+    # In real life, we check the User record and set it's time zone, but we don't have users yet, so we're gonna default
+    # to the dev's time zone.
+
+    Time.use_zone("Pacific Time (US & Canada)") do
+      yield
+    end
+  end
 end
