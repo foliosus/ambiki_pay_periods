0752cb9ce33a3002c63e732c0c835ed7efe8fdc2
Brent Miller <foliosus@foliosus.com>
2024-07-26 13:46:02 -0700

    PayPeriod can handle updates with adjacent periods
    This is critical; pay periods are largely contiguous, with every day of the year being in a period. Changing a period can leave a gap, or overlap its neighbors; this makes adjustments much easier.

--- app/models/pay_period.rb
+++ app/models/pay_period.rb
@@ -15,6 +15,38 @@ class PayPeriod < ApplicationRecord
     end_date.end_of_day
   end
 
+  def update_and_correct_adjacent_pay_periods(new_attribs)
+    saved = false
+
+    PayPeriod.transaction do
+      assign_attributes(new_attribs)
+      # First we delete any periods that have been subsumed by the change
+      PayPeriod.where("start_date >= :start_date AND end_date <= :end_date AND id != :id", start_date: start_date, end_date: end_date, id: id).delete_all
+
+      # Second we load any remaining affected periods, and change their dates
+      in_scope_periods = PayPeriod.inclusive_of_dates([start_date, start_date_was].min - 1, [end_date, end_date_was].max + 1).where.not(id: self.id)
+      in_scope_periods.each do |pay_period|
+        if pay_period.start_date < start_date
+          pay_period.assign_attributes(end_date: start_date - 1)
+        elsif pay_period.end_date > end_date
+          pay_period.assign_attributes(start_date: end_date + 1)
+        end
+      end
+
+      # Last, we check our results. We save without validating, to bypass the
+      # pay_period_does_not_overlap_existing_periods validation, then re-check validity before closing the transaction
+      in_scope_periods.collect{|pp| pp.save(validate: false) }
+      save(validate: false)
+      if in_scope_periods.all?(&:valid?) && valid?
+        saved = true
+      else
+        in_scope_periods.each{|pp| puts "#{pp.start_date} - #{pp.end_date} #{pp.errors.inspect}" }
+        raise ActiveRecord::TransactionRollbackError
+      end
+    end
+    saved
+  end
+
   private def pay_period_does_not_overlap_existing_periods
     query_scope = PayPeriod
     query_scope = query_scope.where.not(id: self.id) if persisted?
--- test/models/pay_period_test.rb
+++ test/models/pay_period_test.rb
@@ -42,4 +42,64 @@ class PayPeriodTest < ActiveSupport::TestCase
       assert_equal expected_time, @pay_period.end_time
     end
   end
+
+  context "multiple updates" do
+    setup do
+      # Four adjacent 3-day pay periods
+      @pay_periods = [
+        create(:pay_period, start_date: Date.today + 0, end_date: Date.today + 2),
+        create(:pay_period, start_date: Date.today + 3, end_date: Date.today + 5),
+        create(:pay_period, start_date: Date.today + 6, end_date: Date.today + 8),
+        create(:pay_period, start_date: Date.today + 9, end_date: Date.today + 11),
+      ]
+      @target_pay_period = @pay_periods[1]
+    end
+
+    should "expand a pay period and adjust the immediate neighbors" do
+      assert @target_pay_period.update_and_correct_adjacent_pay_periods(start_date: @target_pay_period.start_date - 1, end_date: @target_pay_period.end_date + 1), "Should run the update"
+      expected_dates = [
+        [Date.today + 0, Date.today + 1],
+        [Date.today + 2, Date.today + 6],
+        [Date.today + 7, Date.today + 8],
+        [Date.today + 9, Date.today + 11],
+      ]
+      @pay_periods.collect(&:reload)
+      assert_pay_periods_with_dates(@pay_periods, expected_dates)
+    end
+
+    should "shrink a pay period and adjust the immediate neighbors" do
+      assert @target_pay_period.update_and_correct_adjacent_pay_periods(start_date: @target_pay_period.start_date + 1, end_date: @target_pay_period.end_date - 1), "Should run the update"
+      expected_dates = [
+        [Date.today + 0, Date.today + 3],
+        [Date.today + 4, Date.today + 4],
+        [Date.today + 5, Date.today + 8],
+        [Date.today + 9, Date.today + 11],
+      ]
+      @pay_periods.collect(&:reload)
+      assert_pay_periods_with_dates(@pay_periods, expected_dates)
+    end
+
+    should "expand a pay period so much that it 'eats' a neighbor and adjusts the other neighbors" do
+      # This update takes over the 3rd pay period, and shrinks the 4th pay period
+      assert @target_pay_period.update_and_correct_adjacent_pay_periods(end_date: @target_pay_period.end_date + 4), "Should run the update"
+      expected_dates = [
+        [Date.today + 0, Date.today + 2],
+        [Date.today + 3, Date.today + 9],
+        [Date.today + 10, Date.today + 11],
+      ]
+      remaining_pay_periods = @pay_periods[0..1] + @pay_periods[-1..]
+      remaining_pay_periods.collect(&:reload)
+      assert_pay_periods_with_dates(remaining_pay_periods, expected_dates)
+      assert_nil PayPeriod.where(id: @pay_periods[2].id).first, "Should have deleted the 3rd pay period"
+    end
+  end
+
+  private def assert_pay_periods_with_dates(pay_periods, expected_dates)
+    expected_dates.each_with_index do |expected_dates, index|
+      expected_start_date, expected_end_date = expected_dates
+      pay_period = pay_periods[index]
+      assert_equal expected_start_date, pay_period.start_date, "The #{index + 1} period should have a start date of #{expected_start_date}, but has #{pay_period.start_date}"
+      assert_equal expected_end_date, pay_period.end_date, "The #{index + 1} period should have an end date of #{expected_end_date}, but has #{pay_period.end_date}"
+    end
+  end
 end
